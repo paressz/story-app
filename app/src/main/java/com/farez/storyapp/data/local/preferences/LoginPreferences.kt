457e873ba55c673c5aa66f56e0cad3b5c04c9439
paress <farez0830@gmail.com>
2023-05-06 16:17:47 +0700

    add login preferences

--- app/src/main/java/com/farez/storyapp/data/local/preferences/LoginPreferences.kt
+++ app/src/main/java/com/farez/storyapp/data/local/preferences/LoginPreferences.kt
@@ -1,4 +1,57 @@
 package com.farez.storyapp.data.local.preferences
 
-class LoginPreferences {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class LoginPreferences (private val dataStore: DataStore<Preferences>) {
+
+    private val token = stringPreferencesKey("TOKEN")
+    private val auth = booleanPreferencesKey("AUTH")
+
+    fun getToken() : Flow<String> {
+        return dataStore.data.map {
+            it[token] ?: "null"
+        }
+    }
+
+    suspend fun saveToken(t: String) {
+        dataStore.edit {
+            it [token] = t
+        }
+    }
+
+    suspend fun delToken() {
+        dataStore.edit {
+            it[token] = "null"
+        }
+    }
+
+    suspend fun setAuth(b : Boolean) {
+        dataStore.edit {
+            it[auth] = b
+        }
+    }
+
+    fun getAuth(): Flow<Boolean> {
+        return dataStore.data.map {
+            it[auth] ?: false
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LoginPreferences? = null
+        fun getInstance(dataStore: DataStore<Preferences>): LoginPreferences {
+            return INSTANCE ?: synchronized(this) {
+                val instance = LoginPreferences(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
--- app/src/main/java/com/farez/storyapp/ui/activity/login/LoginViewModel.kt
+++ app/src/main/java/com/farez/storyapp/ui/activity/login/LoginViewModel.kt
@@ -1,21 +1,30 @@
 package com.farez.storyapp.ui.activity.login
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.farez.storyapp.data.local.preferences.LoginPreferences
 import com.farez.storyapp.data.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
+class LoginViewModel(private val userRepository: UserRepository, loginPreferences: LoginPreferences) : ViewModel() {
+    val getAuth : LiveData<Boolean> = loginPreferences.getAuth().asLiveData()
+    val token : LiveData<String> = loginPreferences.getToken().asLiveData()
     fun login(email : String, password : String) {
         userRepository.login(email, password)
     }
+    fun saveToken(token : String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            saveToken(token)
+        }
+    }
 }
 
-class LoginVMFactory(private val userRepository: UserRepository) : ViewModelProvider.Factory {
+class LoginVMFactory(private val userRepository: UserRepository, private val loginPreferences: LoginPreferences) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
-            return LoginViewModel(userRepository) as T
+            return LoginViewModel(userRepository, loginPreferences) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
